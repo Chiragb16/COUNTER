8812a30041c205c6f55571d4f19a376e410c6b46
Chiragb16 <152618345+Chiragb16@users.noreply.github.com>
2024-07-28 21:34:48 +0530

    Add files via upload
    Made a simple counter for fun using js , html and css.

---
+++ co.js
@@ -0,0 +1,11 @@
+function sub() {
+    let hi = document.getElementById("hi");
+    let c = parseInt(hi.innerHTML);
+    hi.innerHTML = c - 1;
+}
+
+function add() {
+    let hi = document.getElementById("hi");
+    let c = parseInt(hi.innerHTML);
+    hi.innerHTML = c + 1;
+}
